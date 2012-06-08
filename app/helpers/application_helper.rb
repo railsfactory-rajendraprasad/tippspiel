fe1abe0812c85cb63adeecd421b919b81a477d6f
Holger Arndt <arndt@comcard.de>
2012-06-08 13:40:44 +0200

    Namenskorrektur verschoben und erweitert

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def link_to_new_or_edit_tipp(user_id, spiel_id, txt='Tipp abgeben', xs=false)
+    tipp = Tipp.where(:user_id=>user_id, :spiel_id=>spiel_id).first
+    if tipp
+      txt = (xs) ? tipp.extra_small : tipp.small
+      return link_to(txt, edit_tipp_path(tipp), :class=>'button')
+    else
+      return link_to(txt, new_tipp_path(:spiel_id=>spiel_id,:user_id=>user_id), :class=>'button')
+    end
+  end
+  
+  def nameify(str)
+    str.split(' ').map(&:capitalize).join(' ').gsub('Vdh', 'vdH')
+  end
 end
